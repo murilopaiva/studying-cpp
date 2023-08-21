ce8b60abbdc5a97a0d1a6b76bd123fcc8161b353
murilo paiva <murilopaiva22@gmail.com>
2023-08-21 10:47:58 -0300

    estudando cout

---
+++ letra_J.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout << "A letra " << 'J';
+cout << " pronuncia-se " << "Jota" << '.' << endl;
+system("PAUSE");
+return 0;
+}
---
+++ venus.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout << "\n Venus esta a " << 67 << " milhoes de milhas"
+     << '\n' << "do sol." << endl;
+system("PAUSE");
+return 0;
+}
